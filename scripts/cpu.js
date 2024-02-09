1046ab1e163e05046c0442fa16b51fb8974515cc
Scott Petrovic <scottpetrovic@gmail.com>
2024-02-09 07:33:04 -0600

    Finish adding opcodes

--- scripts/cpu.js
+++ scripts/cpu.js
@@ -359,26 +359,83 @@ class CPU
                         this.v[x] = this.v[y] - this.v[x];
                         break;
                     case 0xE:
-                        // TODO: START HERE - https://www.freecodecamp.org/news/creating-your-very-own-chip-8-emulator/
+                        // 8xyE - SHL Vx {, Vy}
+                        // This instruction not only shifts Vx left 1, but also sets 
+                        // VF to either 0 or 1 depending on if a condition is met.
+                        this.v[0xF] = (this.v[x] & 0x80);
+                        this.v[x] <<= 1; // multiply Vx by 2 by shifting it left 1.
                         break;
                 }
         
                 break;
             case 0x9000:
+                // 9xy0 - SNE Vx, Vy
+                if (this.v[x] !== this.v[y]) {
+                    this.pc += 2; // program counter increment
+                }
                 break;
             case 0xA000:
+                // Annn - LD I, addr
+                // Set the value of register i to nnn. 
+                // If the opcode is 0xA740 then (opcode & 0xFFF) will return 0x740.
+                this.i = (opcode & 0xFFF);
                 break;
             case 0xB000:
+                // Bnnn - JP V0, addr
+                this.pc = (opcode & 0xFFF) + this.v[0];
                 break;
             case 0xC000:
+                // Cxkk - RND Vx, byte
+                // random number in the range 0-255 (0x00-0xFF)
+                let rand = Math.floor(Math.random() * 0xFF);
+                this.v[x] = rand & (opcode & 0xFF);
                 break;
             case 0xD000:
+                // Dxyn - DRW Vx, Vy, nibble
+                // handles the drawing and erasing of pixels on the screen
+                let width = 8; // each sprite is 8 pixels wide
+                let height = (opcode & 0xF); //get last bit of opcode
+            
+                this.v[0xF] = 0;
+            
+                // loop through all the pixels of the sprite with nested loop
+                for (let row = 0; row < height; row++) 
+                {
+                    // grabbing 8-bits of memory
+                    let sprite = this.memory[this.i + row];
+            
+                    for (let col = 0; col < width; col++) 
+                    {
+                        // If the bit (sprite) is not 0, render/erase the pixel
+                        if ((sprite & 0x80) > 0) 
+                        {
+                            // If setPixel returns 1, which means a pixel was erased, set VF to 1
+                            if (this.renderer.setPixel(this.v[x] + col, this.v[y] + row)) {
+                                this.v[0xF] = 1;
+                            }
+                        }
+            
+                        // Shift the sprite left 1. This will move the next next col/bit of the sprite into the first position.
+                        // Ex. 10010000 << 1 will become 0010000
+                        sprite <<= 1;
+                    }
+                }
                 break;
             case 0xE000:
                 switch (opcode & 0xFF) {
                     case 0x9E:
+                        // Ex9E - SKP Vx
+                        // Skip next instruction if key with the value of Vx is pressed
+                        if (this.keyboard.isKeyPressed(this.v[x])) {
+                            this.pc += 2;
+                        }
                         break;
                     case 0xA1:
+                        // ExA1 - SKNP Vx
+                        // ! symbol to negate the value. oppososite of last opcode
+                        if (!this.keyboard.isKeyPressed(this.v[x])) {
+                            this.pc += 2;
+                        }
                         break;
                 }
         
@@ -386,22 +443,65 @@ class CPU
             case 0xF000:
                 switch (opcode & 0xFF) {
                     case 0x07:
+                        // Fx07 - LD Vx, DT
+                        this.v[x] = this.delayTimer;
                         break;
                     case 0x0A:
+                        // Fx0A - LD Vx, K
+                        // pause the emulator until a key is pressed
+                        this.paused = true;
+
+                        this.keyboard.onNextKeyPress = function(key) {
+                            this.v[x] = key;
+                            this.paused = false;
+                        }.bind(this);
                         break;
                     case 0x15:
+                        // Fx15 - LD DT, Vx
+                        // sets the value of the delay timer to the value stored in register Vx
+                        this.delayTimer = this.v[x];
                         break;
                     case 0x18:
+                        // Fx18 - LD ST, Vx
+                        // similar to previous opcde, but set the sound timer
+                        this.soundTimer = this.v[x];
                         break;
                     case 0x1E:
+                        // Fx1E - ADD I, Vx
+                        this.i += this.v[x];
                         break;
                     case 0x29:
+                        // Fx29 - LD F, Vx - ADD I, Vx
+                        // setting I to the location of the sprite at Vx. 
+                        // It's multiplied by 5 because each sprite is 5 bytes long.
+                        this.i = this.v[x] * 5;
                         break;
                     case 0x33:
+                        // Fx33 - LD B, Vx
+                        //  grab the hundreds, tens, and ones digit from register Vx 
+                        // and store them in registers I, I+1, and I+2 
+                        this.memory[this.i] = parseInt(this.v[x] / 100);
+
+                        // Get tens digit and place it in I+1. Gets a value between 0 and 99,
+                        // then divides by 10 to give us a value between 0 and 9.
+                        this.memory[this.i + 1] = parseInt((this.v[x] % 100) / 10);
+
+                        // Get the value of the ones (last) digit and place it in I+2.
+                        this.memory[this.i + 2] = parseInt(this.v[x] % 10);
                         break;
                     case 0x55:
+                        // Fx55 - LD [I], Vx
+                        for (let registerIndex = 0; registerIndex <= x; registerIndex++) {
+                            this.memory[this.i + registerIndex] = this.v[registerIndex];
+                        }
                         break;
                     case 0x65:
+                        // Fx65 - LD Vx, [I]
+                        //  opposite of Fx55. It reads values from memory starting 
+                        // at I and stores them in registers V0 through Vx.
+                        for (let registerIndex = 0; registerIndex <= x; registerIndex++) {
+                            this.v[registerIndex] = this.memory[this.i + registerIndex];
+                        }
                         break;
                 }
         
